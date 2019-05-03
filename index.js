b1c7547b1bfceae316f32957efa4a7b59f214b8e
DANIELLE ALBA <danib.sgh@gmail.com>
2019-05-03 15:14:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 // Add your Circle class here
-class Circle{
-  constructor(radius){
-    this.radius = radius
+class Circle {
+  constructor( radius ) {
+    this.radius = radius;
   }
-  
-  
+
+  get diameter() {
+    return this.radius * 2
+  }
+
+  set diameter( diameter ) {
+    this.radius = diameter / 2
+  }
+
+  get circumference() {
+    return ( this.radius * 2 ) * pi
+  }
+
+  set circumference( circumference ) {
+    this.radius = circumference / ( pi * 2 )
+  }
+
+  get area() {
+    return pi * ( this.radius * this.radius )
+  }
+
+  set area( area ) {
+    this.radius = Math.sqrt( area / pi )
+  }
 }
